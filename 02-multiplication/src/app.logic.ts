57466dc353c244b0bad191a8f05451820e14454c
Andrés Salgado <andressalgado41@gmail.com>
2024-12-12 17:15:35 -0500

    Implement yargs

--- 02-multiplication/src/app.logic.ts
+++ 02-multiplication/src/app.logic.ts
@@ -1,27 +1,27 @@
 import fs from 'node:fs';
+import { yarg } from './config/plugins/args.plugin';
 
-const entrada = 7;
+const { b: base, l: limit, s: showTable } = yarg;
 
-const generarTabla = (entrada: number) => {
+const generarTabla = (base: number, limit: number, showTable: boolean = false) => {
     let salida = '';
 
     salida =
-        '====================== \n       Tabla del ' +
-        entrada +
-        '      \n====================== \n';
+        '====================== \n       Tabla del ' + base + '      \n====================== \n';
 
-    for (let i = 1; i <= 10; i++) {
-        salida += `${entrada} x ${i} = ${entrada * i} \n`;
+    for (let i = 1; i <= limit; i++) {
+        salida += `${base} x ${i} = ${base * i} \n`;
     }
 
-    console.log(salida);
+    if (showTable) console.log(salida);
+
     return salida;
 };
 
 const outputPath = 'outputs';
 
 fs.mkdirSync(outputPath, { recursive: true });
-fs.writeFileSync(`${outputPath}/tabla-${entrada}.txt`, generarTabla(entrada));
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, generarTabla(base, limit, showTable));
 
 console.log('Archivo de Tabla creada');
 
