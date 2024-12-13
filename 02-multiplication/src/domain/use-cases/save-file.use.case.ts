df900489e35cf1979ca67014647c286781f8f7ad
Andrés Salgado <andressalgado41@gmail.com>
2024-12-13 16:43:12 -0500

    Add use case save file

---
+++ 02-multiplication/src/domain/use-cases/save-file.use.case.ts
@@ -0,0 +1,26 @@
+import fs from 'node:fs';
+
+interface SaveFileUseCase {
+    execute: (options: Options) => boolean;
+}
+
+interface Options {
+    fileContent: string;
+    fileDestination?: string;
+    fileName?: string;
+}
+
+export class SaveFile implements SaveFileUseCase {
+    constructor() {}
+
+    execute({ fileContent, fileDestination = 'outputs', fileName = 'table' }: Options): boolean {
+        try {
+            fs.mkdirSync(fileDestination, { recursive: true });
+            fs.writeFileSync(`${fileDestination}/${fileName}.txt`, fileContent);
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+}
