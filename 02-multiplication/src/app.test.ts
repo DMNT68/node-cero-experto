a2f1d82e9237cd973dc5a73d9453331e3e262617
Andrés Salgado <andressalgado41@gmail.com>
2024-12-17 17:39:42 -0500

    Add test app

--- 02-multiplication/src/app.test.ts
+++ 02-multiplication/src/app.test.ts
@@ -1,6 +1,18 @@
-describe('App', () => {
+import { ServerApp } from './presentation/server';
+describe('Test app', () => {
+    test('should call Server.run values', async () => {
+        const serverRunMock = jest.fn();
+        ServerApp.run = serverRunMock;
+        process.argv = ['node', 'app.ts', '-b', '10', '-l', '20', '-s' ,'-n', 'test', '-d', 'test'];
 
-    test('should be 30', () => {
-        expect( 10 + 20 ).toBe(30);
-    })
-})
+        await import('./app');
+
+        expect(serverRunMock).toHaveBeenCalledWith({
+            base: 10,
+            limit: 20,
+            showTable: true,
+            fileDestination: 'test',
+            fileName: 'test',
+        });
+    });
+});
